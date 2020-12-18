29f55c17835ac7370a70879cd2026bbe0146af56
Macbook Marconato <m.lucalmer@gmail.com>
2020-12-18 13:40:26 +0100

    pushing notebooks on the server

--- jupy.py
+++ jupy.py
@@ -7,6 +7,7 @@ os.makedirs('data/spatial_uzh_processed/a/', exist_ok=True)
 
 
 def plt_show(plt, filename):
-    f = os.path.join('data/spatial_uzh/processed/a', filename)
-    plt.savefig(filename)
+    f = os.path.join('data/spatial_uzh_processed/a', filename)
+    plt.savefig(f)
+    print(f'saving figure in {f}')
     plt.show()
