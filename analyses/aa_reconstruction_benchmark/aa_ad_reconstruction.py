ea2bcf64a9467ea9a2ab4609e2e1fd6896a9c7f7
Macbook Marconato <m.lucalmer@gmail.com>
2021-09-12 19:46:54 +0200

    function to ease the comparison of imputation almost finished

--- analyses/aa_reconstruction_benchmark/aa_ad_reconstruction.py
+++ analyses/aa_reconstruction_benchmark/aa_ad_reconstruction.py
@@ -1,7 +1,7 @@
 ##
+from __future__ import annotations
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
-from scipy.stats import kde
 import time
 import torch
 from sklearn.metrics import adjusted_rand_score
@@ -60,12 +60,12 @@ from analyses.ab_images_vs_expression.ab_aa_expression_latent_samples import (
 
 if m:
     MODEL_CHECKPOINT = "/data/l989o/deployed/a/data/spatial_uzh_processed/a/checkpoints/expression_vae/version_128/checkpoints/last.ckpt"
-b0, b1 = precompute(
-    data_loaders=get_loaders(perturb=True),
-    expression_model_checkpoint=MODEL_CHECKPOINT,
-    random_indices=random_indices,
-    split=SPLIT,
-)
+    b0, b1 = precompute(
+        data_loaders=get_loaders(perturb=True),
+        expression_model_checkpoint=MODEL_CHECKPOINT,
+        random_indices=random_indices,
+        split=SPLIT,
+    )
 ##
 if m:
     louvain_plot(b1, "expression (perturbed)")
@@ -102,9 +102,9 @@ if m:
     all_is_perturbed = []
     all_expression_non_perturbed = []
     for data, data_non_perturbed in tqdm(
-        zip(loader, loader_non_perturbed),
-        desc="embedding expression",
-        total=len(loader),
+            zip(loader, loader_non_perturbed),
+            desc="embedding expression",
+            total=len(loader),
     ):
         expression, _, is_perturbed = data
         expression_non_perturbed, _, _ = data_non_perturbed
@@ -159,102 +159,262 @@ if m:
     plt.ylabel("score")
     plt.show()
 
+##
 
 ##
-def plot_imputation(imputed, original, ax):  # , zeros, i, j, ix, xtext):
-    # all_index = i[ix], j[ix]
-    # x, y = imputed[all_index], original[all_index]
-    #
-    # x = x[zeros[all_index] == 0]
-    # y = y[zeros[all_index] == 0]
-    #
-    q = 0.9
-    cutoff = max(np.quantile(original, q), np.quantile(imputed, q))
-    mask = imputed < cutoff
-    imputed = imputed[mask]
-    original = original[mask]
+from enum import IntEnum
+from typing import Callable
+from data2 import quantiles_for_normalization
 
-    mask = original < cutoff
-    imputed = imputed[mask]
-    original = original[mask]
+Space = IntEnum("Space", "raw asinh scaled", start=0)
 
-    l = np.minimum(imputed.shape[0], original.shape[0])
+from_to = [[None for _ in range(len(Space))] for _ in range(len(Space))]
 
-    assert len(imputed) == len(original)
-    imputed = imputed[:l]
-    original = original[:l]
 
-    # data = np.vstack([x, y])
-    data = np.vstack([imputed, original])
+def f_set(space0: Space, space1: Space, transformation: Callable):
+    from_to[space0.value][space1.value] = transformation
 
-    ax.set_xlim([0, cutoff])
-    ax.set_ylim([0, cutoff])
 
-    nbins = 50
+def f_get(space0: Space, space1: Space):
+    return from_to[space0.value][space1.value]
 
-    # Evaluate a gaussian kde on a regular grid of nbins x nbins over data extents
-    k = kde.gaussian_kde(data)
-    xi, yi = np.mgrid[0 : cutoff : nbins * 1j, 0 : cutoff : nbins * 1j]
 
-    start = time.time()
-    zi = k(np.vstack([xi.flatten(), yi.flatten()]))
-    # print(f"evaluating the kernel on the mesh: {time.time() - start}")
-
-    ax.pcolormesh(yi, xi, zi.reshape(xi.shape), cmap="Reds", shading="gouraud")
-
-    a, _, _, _ = np.linalg.lstsq(original[:, np.newaxis], imputed, rcond=None)
-    l = np.linspace(0, cutoff)
-    ax.plot(l, a * l, color="black")
-
-    # A = np.vstack([original, np.ones(len(original))]).T
-    # aa, _, _, _ = np.linalg.lstsq(A, imputed, rcond=None)
-    # ax.plot(l, aa[0] * l + aa[1], color="red")
-
-    ax.plot(l, l, color="black", linestyle=":")
-
+f_set(Space.raw, Space.raw, lambda x: x)
+f_set(Space.asinh, Space.asinh, lambda x: x)
+f_set(Space.scaled, Space.scaled, lambda x: x)
+f_set(Space.raw, Space.asinh, lambda x: np.arcsinh(x))
+f_set(Space.asinh, Space.raw, lambda x: np.sinh(x))
+f_set(Space.asinh, Space.scaled, lambda x: x / quantiles_for_normalization)
+f_set(Space.scaled, Space.asinh, lambda x: x * quantiles_for_normalization)
+f_set(
+    Space.raw,
+    Space.scaled,
+    lambda x: f_get(Space.asinh, Space.scaled)(f_get(Space.raw, Space.asinh)(x)),
+)
+f_set(
+    Space.scaled,
+    Space.raw,
+    lambda x: f_get(Space.asinh, Space.raw)(f_get(Space.scaled, Space.asinh)(x)),
+)
 
 ##
-def plot_reconstruction(original_non_perturbed, reconstructed_zero, n_channels):
-    from matplotlib.lines import Line2D
+import math
+from scipy.stats import t as t_dist
 
-    d = 3
-    fig, axes = plt.subplots(5, 8, figsize=(8 * d, 5 * d))
-    axes = axes.flatten()
 
-    custom_lines = [
-        Line2D([0], [0], color="black", linestyle=":", lw=1),
-        Line2D([0], [0], color="black", lw=1),
-        # Line2D([0], [0], color="red", lw=1),
-    ]
+class Prediction:
+    def __init__(
+            self,
+            original: np.ndarray,
+            corrupted_entries: np.ndarray,
+            predictions_from_perturbed: np.ndarray,
+            space: Space,
+            name: str
+    ):
+        self.original = original
+        self.corrupted_entries = corrupted_entries
+        self.predictions_from_perturbed = predictions_from_perturbed
+        self.space = space
+        self.name = name
+        self.scores_non_perturbed = None
+        self.scores_perturbed = None
+        assert self.original.shape == self.corrupted_entries.shape
+        assert self.original.shape == self.predictions_from_perturbed.shape
 
-    axes[0].legend(
-        custom_lines,
-        [
-            "identity",
-            "linear model",
-            # "affine model"
-        ],
-        loc="center",
-    )
-    axes[0].set_axis_off()
-
-    for i in tqdm(range(n_channels), desc="channels"):
-        ax = axes[i + 1]
-        original = original_non_perturbed[i].detach().numpy()
-        imputed = reconstructed_zero[i].detach().numpy()
-        plot_imputation(
-            original=original,
-            imputed=imputed,
-            ax=ax,
+    def transform_to(self, space: Space) -> Prediction:
+        f = f_get(self.space, space)
+        p = Prediction(
+            original=f(self.original),
+            corrupted_entries=self.corrupted_entries,
+            predictions_from_perturbed=f(self.predictions_from_perturbed),
+            space=space,
+            name=self.name
         )
-        score = np.median(np.abs(original - imputed))
-        ax.set(title=f"ch {i}, score: {score:0.2f}")
-        if i == 0:
-            ax.set(xlabel="original", ylabel="imputed")
-        # if i > 2:
-        #     break
-    plt.tight_layout()
-    plt.show()
+        return p
+
+    def compute_scores(self):
+        ce = self.corrupted_entries
+        ne = np.logical_not(self.corrupted_entries)
+
+        uu0 = self.predictions_from_perturbed[ce]
+        uu1 = self.original[ce]
+
+        vv0 = self.predictions_from_perturbed[ne]
+        vv1 = self.original[ne]
+
+        self.scores_perturbed = np.abs(uu0 - uu1)
+        self.scores_non_perturbed = np.abs(vv0 - vv1)
+
+    def check_scores_defined(self, test=False):
+        b = self.scores_perturbed is not None and self.scores_non_perturbed is not None
+        if test:
+            return b
+        else:
+            assert b
+
+    def plot_scores(self):
+        self.check_scores_defined()
+
+        s = self.scores_perturbed
+        t = self.scores_non_perturbed
+
+        # corrupted entries, normal entries
+        fig = plt.figure(figsize=(10, 5))
+        plt.subplot(1, 2, 1)
+        plt.hist(s)
+        m_s = np.median(s)
+        plt.title(f"scores for imputed entries\nmedian: {m_s:0.2f}")
+        plt.yscale("log")
+
+        plt.subplot(1, 2, 2)
+        plt.hist(t)
+        m_t = np.median(t)
+        plt.title(f"control: normal entries\nmedian: {m_t:0.2f}")
+        plt.yscale("log")
+
+        fig.suptitle(f"{self.name}: abs(original vs predicted)")
+        plt.tight_layout()
+        plt.show()
+
+        return m_s, s
+
+    @staticmethod
+    def welch_t_test(abs_values0: np.ndarray, abs_values1: np.ndarray):
+        n = min(len(abs_values0), len(abs_values1))
+        # s and t here are in general different from the s and t in the function compute_and_plot_scores
+        s = np.random.choice(abs_values0, n, replace=False)
+        t = np.random.choice(abs_values1, n, replace=False)
+
+        # Welch's t test
+        sem_s = s.std() / math.sqrt(len(s))
+        sem_t = t.std() / math.sqrt(len(t))
+        sed = math.sqrt(sem_s ** 2 + sem_t ** 2)
+        t_statistic = (s.mean() - t.mean()) / sed
+
+        # degrees of freedom from the Welch-Satterthwaite equation
+        delta = s - t
+        sem_delta = delta.std() / math.sqrt(len(delta))
+        df = sem_delta ** 4 / (
+                (len(s) - 1) ** -1 * sem_s ** 4 + (len(t) - 1) ** -1 * sem_t ** 4
+        )
+        # print(f"df = {df}")
+
+        p_value = 1 - t_dist.cdf(t_statistic, df)
+        print(f"welch's t test: p_value = {p_value}")
+
+    @staticmethod
+    def plot_imputation(imputed, original, ax):  # , zeros, i, j, ix, xtext):
+        from scipy.stats import kde
+        import time
+        # all_index = i[ix], j[ix]
+        # x, y = imputed[all_index], original[all_index]
+        #
+        # x = x[zeros[all_index] == 0]
+        # y = y[zeros[all_index] == 0]
+        #
+        q = 0.9
+        cutoff = max(np.quantile(original, q), np.quantile(imputed, q))
+        mask = imputed < cutoff
+        imputed = imputed[mask]
+        original = original[mask]
+
+        mask = original < cutoff
+        imputed = imputed[mask]
+        original = original[mask]
+
+        l = np.minimum(imputed.shape[0], original.shape[0])
+
+        assert len(imputed) == len(original)
+        imputed = imputed[:l]
+        original = original[:l]
+
+        # data = np.vstack([x, y])
+        data = np.vstack([imputed, original])
+
+        ax.set_xlim([0, cutoff])
+        ax.set_ylim([0, cutoff])
+
+        nbins = 50
+
+        # Evaluate a gaussian kde on a regular grid of nbins x nbins over data extents
+        k = kde.gaussian_kde(data)
+        xi, yi = np.mgrid[0: cutoff: nbins * 1j, 0: cutoff: nbins * 1j]
+
+        start = time.time()
+        zi = k(np.vstack([xi.flatten(), yi.flatten()]))
+        # print(f"evaluating the kernel on the mesh: {time.time() - start}")
+
+        ax.pcolormesh(yi, xi, zi.reshape(xi.shape), cmap="Reds", shading="gouraud")
+
+        a, _, _, _ = np.linalg.lstsq(original[:, np.newaxis], imputed, rcond=None)
+        l = np.linspace(0, cutoff)
+        ax.plot(l, a * l, color="black")
+
+        # A = np.vstack([original, np.ones(len(original))]).T
+        # aa, _, _, _ = np.linalg.lstsq(A, imputed, rcond=None)
+        # ax.plot(l, aa[0] * l + aa[1], color="red")
+
+        ax.plot(l, l, color="black", linestyle=":")
+
+    # def plot_reconstruction(original_non_perturbed, reconstructed_zero, n_channels):
+    def plot_reconstruction(self):
+        if not self.check_scores_defined(test=True):
+            self.compute_scores()
+
+        n_channels = self.original.shape[-1]
+        original_non_perturbed = []
+        reconstructed_zero = []
+        for i in range(n_channels):
+            x = self.original[:, i][self.corrupted_entries[:, i]]
+            original_non_perturbed.append(x)
+            x = self.predictions_from_perturbed[:, i][self.corrupted_entries[:, i]]
+            reconstructed_zero.append(x)
+
+        from matplotlib.lines import Line2D
+
+        d = 3
+        fig, axes = plt.subplots(5, 8, figsize=(8 * d, 5 * d))
+        axes = axes.flatten()
+
+        custom_lines = [
+            Line2D([0], [0], color="black", linestyle=":", lw=1),
+            Line2D([0], [0], color="black", lw=1),
+            # Line2D([0], [0], color="red", lw=1),
+        ]
+
+        axes[0].legend(
+            custom_lines,
+            [
+                "identity",
+                "linear model",
+                # "affine model"
+            ],
+            loc="center",
+        )
+        axes[0].set_axis_off()
+
+        for i in tqdm(range(n_channels), desc="channels"):
+            ax = axes[i + 1]
+            original = original_non_perturbed[i]
+            imputed = reconstructed_zero[i]
+            Prediction.plot_imputation(
+                original=original,
+                imputed=imputed,
+                ax=ax,
+            )
+            score = np.median(np.abs(original - imputed))
+            ax.set(title=f"ch {i}, score: {score:0.2f}")
+            if i == 0:
+                ax.set(xlabel="original", ylabel="imputed")
+            # if i > 2:
+            #     break
+        plt.tight_layout()
+        plt.show()
 
 
-##
+def compare_predictions(p0: Prediction, p1: Prediction, target_space: Space):
+    q0 = p0.transform_to(target_space)
+    q1 = p1.transform_to(target_space)
+    m0, s0 = q0.compute_and_plot_scores()
+    m1, s1 = q1.compute_and_plot_scores()
+    Prediction.welch_t_test(s0, s1)
--- analyses/ac_third_party/ac_aa_scvi.py
+++ analyses/ac_third_party/ac_aa_scvi.py
@@ -1,4 +1,8 @@
 ##
+from __future__ import annotations
+
+import sys
+
 import scvi
 import scanpy as sc
 from torch.utils.data import DataLoader
@@ -252,126 +256,87 @@ x_val_perturbed[ce_val] = 0
 a_val_perturbed = ad.AnnData(x_val_perturbed)
 
 ##
-u = model.get_likelihood_parameters(a_val_perturbed)
-mu = u['mean']
+p = model.get_likelihood_parameters(a_val_perturbed)
+from scvi.core.distributions import ZeroInflatedNegativeBinomial
+
+x_val_perturbed_pred = ZeroInflatedNegativeBinomial(
+    mu=torch.tensor(p["mean"]),
+    theta=torch.tensor(p["dispersions"]),
+    zi_logits=torch.tensor(p["dropout"]),
+).mean.numpy()
 
 ##
-# we need some hacking ;-)
-model.get_normalized_expression
-model.get_elbo
-adata = a_val_perturbed
-adata = model._validate_anndata(adata)
-scdl = model._make_scvi_dl(adata=adata, indices=None, batch_size=None)
+# normal entries
+ne_train = np.logical_not(ce_train)
+ne_val = np.logical_not(ce_val)
+x_val = a_val.X.copy()
 
-from scvi.core._log_likelihood import _CONSTANTS
-import torch
+uu0 = x_val_perturbed_pred[ce_val]
+uu1 = x_val[ce_val]
 
-
-def my_compute_elbo(vae, data_loader, feed_labels=True, **kwargs):
-    """
-    Computes the ELBO.
-
-    The ELBO is the reconstruction error + the KL divergences
-    between the variational distributions and the priors.
-    It differs from the marginal log likelihood.
-    Specifically, it is a lower bound on the marginal log likelihood
-    plus a term that is constant with respect to the variational distribution.
-    It still gives good insights on the modeling of the data, and is fast to compute.
-    """
-    # Iterate once over the data and compute the elbo
-    elbo = 0
-    for i_batch, tensors in enumerate(data_loader):
-        sample_batch = tensors[_CONSTANTS.X_KEY]
-        local_l_mean = tensors[_CONSTANTS.LOCAL_L_MEAN_KEY]
-        local_l_var = tensors[_CONSTANTS.LOCAL_L_VAR_KEY]
-        batch_index = tensors[_CONSTANTS.BATCH_KEY]
-        if feed_labels:
-            labels = tensors[_CONSTANTS.LABELS_KEY]
-        else:
-            labels = None
-
-        # kl_divergence_global (scalar) should be common across all batches after training
-        reconst_loss, kl_divergence, kl_divergence_global = vae(
-            sample_batch,
-            local_l_mean,
-            local_l_var,
-            batch_index=batch_index,
-            y=labels,
-            **kwargs
-        )
-        elbo += torch.sum(reconst_loss + kl_divergence).item()
-    n_samples = len(data_loader.indices)
-    elbo += kl_divergence_global
-
-    # return elbo / n_samples
-    # NOW HACKING
-    outputs = vae.inference(x=sample_batch, batch_index=batch_index, y=labels)
-    x = sample_batch
-    px_rate = outputs["px_rate"]
-    px_r = outputs["px_r"]
-    px_dropout = outputs["px_dropout"]
-    from scvi.core.distributions import (
-        NegativeBinomial,
-        ZeroInflatedNegativeBinomial,
-    )
-    from torch.distributions import Poisson
-    # Reconstruction Loss
-    if vae.gene_likelihood == "zinb":
-        mean = (
-            -ZeroInflatedNegativeBinomial(
-                mu=px_rate, theta=px_r, zi_logits=px_dropout
-            )
-                .mean
-        )
-    elif vae.gene_likelihood == "nb":
-        mean = (
-            -NegativeBinomial(mu=px_rate, theta=px_r).mean
-        )
-    elif vae.gene_likelihood == "poisson":
-        mean = -Poisson(px_rate).mean
-    print('the mean:', mean)
-    return elbo / n_samples, mean
-
-
-my_elbo, my_mean = my_compute_elbo(scdl.model, scdl)
-my_elbo = -my_elbo
-elbo = model.get_elbo(adata)
-print(f'my_elbo = {my_elbo}, elbo = {elbo}')
-assert np.isclose(my_elbo, elbo)
-print(f'my_mean.shape = {my_mean.shape}')
+vv0 = x_val_perturbed_pred[ne_val]
+vv1 = x_val[ne_val]
 ##
-model.get_normalized_expression(adata)
-##
-import inspect
-print(inspect.getsource(scdl.model.forward))
-print(inspect.getfile(scdl.model.forward))
-import scvi.core.modules.vae
-##
-u = model.get_normalized_expression(adata).to_numpy()
-uu0 = u[ce_val]
-uu1 = u[np.logical_not(ce_val)]
-print(len(uu0) + len(uu1))
-print(np.prod(adata.shape))
-x = a_val.X
-vv0 = x[ce_val]
-vv1 = x[np.logical_not(ce_val)]
-zz0 = u[ce_val]
-zz1 = u[np.logical_not(ce_val)]
-print(f'vv0.shape = {vv0.shape}, vv1.shape = {vv1.shape}, zz0.shape = {zz0.shape}, zz1.shape = {zz1.shape}')
-##
-plt.figure()
-plt.hist(np.abs(vv0 - zz0))
-plt.loglog()
+fig = plt.figure(figsize=(10, 5))
+plt.subplot(1, 2, 1)
+plt.hist(np.abs(uu0 - uu1))
+m = np.median(np.abs(uu0 - uu1))
+plt.title(f"scores for imputed entries\nmedian: {m:0.2f}")
+plt.yscale("log")
+
+plt.subplot(1, 2, 2)
+plt.hist(np.abs(vv0 - vv1))
+m = np.median(np.abs(vv0 - vv1))
+plt.title(f"control: normal entries\nmedian: {m:0.2f}")
+plt.yscale("log")
+
+fig.suptitle("abs(original vs predicted)")
+plt.tight_layout()
 plt.show()
-plt.figure()
-plt.hist(np.abs(vv1 - zz1))
-plt.loglog()
-plt.show()
+
 ##
-print(np.mean(np.abs(vv0 - zz0)))
-print(np.mean(np.abs(vv1 - zz1)))
+import math
+
+s = np.abs(uu0 - uu1)
+t = np.abs(vv0 - vv1)
+t = np.random.choice(t, len(s), replace=False)
+
+# Welch's t test
+sem_s = s.std() / math.sqrt(len(s))
+sem_t = t.std() / math.sqrt(len(t))
+sed = math.sqrt(sem_s ** 2 + sem_t ** 2)
+t_statistic = (s.mean() - t.mean()) / sed
+
+# degrees of freedom from the Welch-Satterthwaite equation
+delta = s - t
+sem_delta = delta.std() / math.sqrt(len(delta))
+df = sem_delta ** 4 / (
+        (len(s) - 1) ** -1 * sem_s ** 4 + (len(t) - 1) ** -1 * sem_t ** 4
+)
+print(f"df = {df}")
+
+from scipy.stats import t as t_dist
+
+p_value = 1 - t_dist.cdf(t_statistic, df)
+print(f"p_value = {p_value}")
+
+# conclusion: the score for imputed data is worse than the one from non-perturbed data; this is expected and the
+# alternative case would have been a model whose scores are both bad because it is not properly trained
 ##
 
-type(x)
-type(u)
-u.to_numpy().shape
+##
+# from analyses.aa_reconstruction_benchmark.aa_ad_reconstruction import Prediction
+
+scvi_predictions = Prediction(
+    original=x_val,
+    corrupted_entries=ce_val,
+    predictions_from_perturbed=x_val_perturbed_pred,
+    space=Space.raw,
+    name='scVI'
+)
+
+scvi_predictions.plot_reconstruction()
+
+##
+p = scvi_predictions.transform_to(Space.scaled)
+p.plot_reconstruction()
